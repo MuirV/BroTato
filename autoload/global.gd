9a7c7ad342da90f7624103cc2f1db2fe11848419
MuirV <180142614+MuirV@users.noreply.github.com>
2026-05-30 23:25:45 +0800

    feat: 受击shader反馈

--- autoload/global.gd
+++ autoload/global.gd
@@ -1,3 +1,5 @@
 extends Node
 
+const FLASH_MATERIAL = preload("uid://b6nlq50nu48th")
+
 var player: Player
--- scenes/unit.gd
+++ scenes/unit.gd
@@ -7,15 +7,25 @@ class_name Unit
 @onready var sprite: Sprite2D = %Sprite
 @onready var anim_player: AnimationPlayer = $AnimationPlayer
 @onready var health_component: HealthComponent = $HealthComponent
+@onready var flash_timer: Timer = $FlashTimer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	health_component.setup(stats)
 
+func set_flash_material() -> void:
+	sprite.material = Global.FLASH_MATERIAL
+	flash_timer.start()
 
 func _on_hurtbox_component_on_damaged(hitbox: HitboxComponent) -> void:
 	if health_component.current_health <= 0:
 		return
 	
+	set_flash_material()
+	
 	health_component.take_damage(hitbox.damage)
 	print("%s: %d" % [name, health_component.current_health])
+
+
+func _on_flash_timer_timeout() -> void:
+	sprite.material = null
